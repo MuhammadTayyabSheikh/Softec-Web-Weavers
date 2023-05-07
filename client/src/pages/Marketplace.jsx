765da6f007c420eea078e537de00af8b8996a6f4
Salman Wasi <salmanwasi8@gmail.com>
2023-05-07 09:52:30 +0500

    state

--- client/src/pages/Marketplace.jsx
+++ client/src/pages/Marketplace.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Filters, Results, Search } from '../components/marketplace';
+import { useLocation } from 'react-router-dom';
 
 function Marketplace(props) {
+  const { state } = useLocation();
   const [price, setPrice] = useState({
     min: 0,
     max: 100000,
@@ -10,11 +12,25 @@ function Marketplace(props) {
   const [categories, setCategories] = useState([]);
   const [titleSearch, setTitleSearch] = useState('');
 
+  useEffect(() => {
+    if (state.type) {
+      // do update type
+    }
+    if (state.category) {
+      setCategories([state.category]);
+    }
+  }, [state]);
+
   return (
     <div className='px-5 background-dark pb-10 pt-5'>
       <div className='row'>
         <Search />
-        <Filters price={price} setPrice={setPrice} categories={categories} setCategories={setCategories} />
+        <Filters
+          price={price}
+          setPrice={setPrice}
+          categories={categories}
+          setCategories={setCategories}
+        />
         <Results />
       </div>
     </div>
