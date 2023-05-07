ce02e8abdf0415b2d34df3511176c533b434b14a
Salman Wasi <salmanwasi8@gmail.com>
2023-05-07 10:07:49 +0500

    Route protection

--- client/src/routes/index.jsx
+++ client/src/routes/index.jsx
@@ -1,16 +1,24 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import AdminRouter from "./AdminRouter";
-import ClientRouter from "./ClientRouter";
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import AdminRouter from './AdminRouter';
+import AdminRoute from './AdminRoute';
+import ClientRouter from './ClientRouter';
 
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/admin/*" element={<AdminRouter />} />
-        <Route path="*" element={<ClientRouter />} />
+        <Route
+          path='/admin/*'
+          element={
+            <AdminRoute>
+              <AdminRouter />
+            </AdminRoute>
+          }
+        />
+        <Route path='*' element={<ClientRouter />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
 export default AppRouter;
